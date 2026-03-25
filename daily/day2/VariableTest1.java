2b2d0e9b88ecb11a8156207c9802dc7163485eed
yxljtcnq <m19880789869@163.com>
2026-03-25 11:25:04 +0800

    添加旧代码并调整文件夹结构

---
+++ daily/day2/VariableTest1.java
@@ -0,0 +1,7 @@
+public class VariableTest{
+    //主入口
+    public static void main(String[] args){
+        //太简单了没有写P21
+
+    }
+}
---
+++ daily/day2/VariableTest2.java
@@ -0,0 +1,11 @@
+public class VariableTest2{
+    //主入口
+    public static void main(String[] args){
+        //依旧简单
+        int a=10;
+        System.out.println(a);
+        String hi="积极寂寞寂寞";
+        System.out.println(hi);
+    }
+}
+frist
