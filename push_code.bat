a92a4d7f71e83ba30c115381358e72e4c25ea62d
yxljtcnq <m19880789869@163.com>
2026-03-26 18:44:26 +0800

    feat: 添加P37双向链表代码 & fix: 提交脚本乱码问题

--- push_code.bat
+++ push_code.bat
@@ -1,15 +1,23 @@
 @echo off
-echo 自动同步：改名 / 新增 / 修改
-cd /d "%~dp0"
+chcp 65001 >nul
+echo ======================================
+echo          一键提交代码到Gitee
+echo ======================================
+echo.
 
-:: 自动识别改名、新增、改代码
+echo [1/3] 正在添加所有修改的文件...
 git add .
 
-:: 提交
-git commit -m "update: add/rename/edit code" 2>nul
+echo.
+echo [2/3] 正在提交代码...
+set /p "msg=请输入提交备注（直接回车默认：update code）："
+if "%msg%"=="" set "msg=update code"
+git commit -m "%msg%"
 
-:: 推送
-git push
+echo.
+echo [3/3] 正在推送到远程仓库...
+git push origin master
 
-echo ✅ 完成，.gitignore 自动挡住所有垃圾
+echo.
+echo ✅ 代码提交完成！
 pause
