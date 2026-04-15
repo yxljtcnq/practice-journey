8cf0710d02700f914b193b571fd488b9067c8415
yxljtcnq <m19880789869@163.com>
2026-04-15 12:48:04 +0800

    学了继承和继承的重写,写了代码

---
+++ daily/src/AdvancedObjectOrientedProgramming/Inheritance/InheritanceContainsRewrites/Cook.java
@@ -0,0 +1,16 @@
+package AdvancedObjectOrientedProgramming.Inheritance.InheritanceContainsRewrites;
+
+public class Cook extends Employee {
+        public Cook() {
+        }
+
+        public Cook(String id, String name, double salary) {
+            super(id, name, salary);
+        }
+
+
+        @Override//重写都加上这个,优雅且不容易出错
+        public void work() {
+            System.out.println("厨师正在炒菜");
+        }
+}
